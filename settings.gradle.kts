c2ae373cd51edd2095da79b09dfd6fa345e2560a
Kyla Ramoza <158235592+KylaRamoza@users.noreply.github.com>
2024-02-21 10:56:53 +0800

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ramoza_Profile_App"
+include(":app")
+include(":app")
+include(":app")
+ 
